5e3bac69018958b3294bd24659e8c7e4447964f7
adilrazakhan <159728598+adilrazakhan@users.noreply.github.com>
2024-04-18 01:14:56 +0500

    Add files via upload
    Unique Number Guess Game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please Guess a Number:",
+    },
+]);
+if (answer.userGuessNumber === randomNumber){
+    console.log("Congratuation! You Guessed Right Number,");
+} else {
+    console.log("You Guesses Wrong Number.");
+}
